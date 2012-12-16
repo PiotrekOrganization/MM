4ac20fbaa0ad26f0e8127a7ca4866f110e64dc32
Piotr Imbierowicz <piotr.imbierowicz@inkworks.pl>
2012-12-16 23:09:21 +0100

    Init

---
+++ Mindmap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindmap::Application.config.secret_token = 'ed873c034d35e52a76d73a0c99ac6a377d93e0954c1db0419a4204c473e431ce5aea46984d0999709096d5440b5bb6ef0abb76646d870755f0d7cd930f4f1a1c'
